a1a5c27754fa2447130421d6824aed5b48870b2c
NakajimaShinya <nakajima.shinya11@gmail.com>
2017-06-27 23:30:58 +0900

    APIでリストを取得、表示、構造改善

--- app/assets/javascripts/timesheets.js
+++ app/assets/javascripts/timesheets.js
@@ -50,22 +50,24 @@ jQuery(function() {
         init: function() {
 
         },
-        getAll: function() {
-            return $.ajax({
+        getAll: function(params, successFunc, errorFunc) {
+            $.ajax({
                 type: 'GET',
-                url: 'http://localhost:3000/api/v1/timesheets/201706',
-                data: {},
-                dataType: 'json',
-                success: function(response) {
-                    return response.timesheets
-                }
+                url: '/api/v1/timesheets/' + params.yyyymm,
+                dataType: 'json'
+            })
+            .done(function(data, statusCode, jqXHR) {
+                successFunc(data, statusCode, jqXHR);
+            })
+            .fail(function(jqXHR, statusCode, error) {
+                errorFunc(jqXHR, statusCode, error)
             })
         }
     }
 
     var TimesheetCtrl = {
-        getTimesheets: function() {
-            return TimesheetModel.getAll();
+        getTimesheets: function(params, successFunc, errorFunc) {
+            return TimesheetModel.getAll(params, successFunc, errorFunc);
         },
         init: function() {
             TimesheetModel.init();
@@ -77,47 +79,46 @@ jQuery(function() {
         init: function() {
             this.timesheetList = $('#timesheet-list');
             this.bindEvents();
-            this.render();
-            // this.rowSlideIn();
+            if ($('#timesheets-index').length) {
+                this.loadList();
+            }
         },
         bindEvents: function() {
             $('.up-btn').on('click', this.changeTargetBoxNum.bind(this));
             $('.down-btn').on('click', this.changeTargetBoxNum.bind(this));
             $('#timesheet-submit').on('click', this.sendNotification.bind(this));
         },
-        render: function() {
+        afterListLoaded: function(data, statusCode, jqXHR) {
             var htmlStr = '';
-            $.ajax({
-                type: 'GET',
-                url: 'http://localhost:3000/api/v1/timesheets/201706',
-                data: {},
-                dataType: 'json',
-                success: function(response) {
-                    response.timesheets.forEach(function(t){
-                        var start_time = new Date(t.start_time)
-                        var start_time_str = start_time.getHours().toString() + ':' + start_time.getMinutes().toString();
-                        var end_time = new Date(t.end_time)
-                        var end_time_str = end_time.getHours().toString() + ':' + end_time.getMinutes().toString();
+            data.timesheets.forEach(function(t){
+                var start_time = new Date(t.start_time)
+                var start_time_str = start_time.getHours().toString() + ':' + start_time.getMinutes().toString();
+                var end_time = new Date(t.end_time)
+                var end_time_str = end_time.getHours().toString() + ':' + end_time.getMinutes().toString();
 
-                        htmlStr += '<div class="row timesheet-detail">' +
-                                        '<div class="col-xs-2 text-center">' + t.day + '</div>' +
-                                        '<div class="col-xs-2 text-center">' + start_time_str + '</div>' +
-                                        '<div class="col-xs-2 text-center">' + end_time_str + '</div>' +
-                                        '<div class="col-xs-2"></div>' +
-                                        '<div class="col-xs-4 text-right">' +
-                                            '<a href="/timesheets/201706/' + t.day +'/edit" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-pencil"></span></a>' +
-                                            '<a href="/timesheets/' + t.id +'" data-method="delete" class="btn btn-sm btn-danger" style="margin-left: 10px;"><span class="glyphicon glyphicon-trash"></span></a>' +
-                                        '</div>' +
-                                    '</div>';
-                    });
-                    TimesheetView.timesheetList.html(htmlStr);
-                    TimesheetView.rowSlideIn();
-                }
-            })
-
-            // TimesheetCtrl.getTimesheets()
-            this.timesheetList.html(htmlStr);
-            this.rowSlideIn();
+                htmlStr += '<div class="row timesheet-detail">' +
+                    '<div class="col-xs-2 text-center">' + t.day + '</div>' +
+                    '<div class="col-xs-2 text-center">' + start_time_str + '</div>' +
+                    '<div class="col-xs-2 text-center">' + end_time_str + '</div>' +
+                    '<div class="col-xs-2"></div>' +
+                    '<div class="col-xs-4 text-right">' +
+                    '<a href="/timesheets/' + TimesheetView.targetYearMonth + '/' + t.day +'/edit" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-pencil"></span></a>' +
+                    '<a href="/timesheets/' + t.id +'" data-method="delete" class="btn btn-sm btn-danger" style="margin-left: 10px;"><span class="glyphicon glyphicon-trash"></span></a>' +
+                    '</div>' +
+                    '</div>';
+            });
+            TimesheetView.timesheetList.html(htmlStr);
+            TimesheetView.rowSlideIn();
+        },
+        errorListLoaded: function(jqXHR, statusCode, error) {
+            alert('Load Error!');
+        },
+        loadList: function() {
+            this.targetYearMonth = $('#target-year-month').val();
+            var params = {
+                yyyymm: this.targetYearMonth
+            }
+            TimesheetCtrl.getTimesheets(params, this.afterListLoaded, this.errorListLoaded);
         },
         rowSlideIn: function() {
             $('.timesheet-detail').each(function(i) {
